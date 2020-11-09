99a6b34da6e874f104456144905466c19e20543e
GlycineASoya <ivlevinsky@gmail.com>
2020-11-09 12:48:54 +0100

    Update eve-ng_aws_metal.sh

--- eve-ng_aws_metal.sh
+++ eve-ng_aws_metal.sh
@@ -21,20 +21,31 @@
 #userdel -r <username>
 
 #The server side
+apt-get update -y && \
+apt-get install -y make && \
+apt-get install -y wget && \
+apt-get install -y gcc && \
+apt-get install -y vim && \
+apt-get install -y expect
+
+#CREATING IN SKELETON FOR NEW USERS .SSH DIR
 mkdir /etc/skel/.ssh
+
+#ENABLING WHEEL
 sed -i -e 's/# auth       required   pam_wheel.so/ auth       required   pam_wheel.so/' /etc/pam.d/su
-sed -i -e 's/PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
-sed -i -e 's/#AuthorizedKeysFile/AuthorizedKeysFile' /etc/ssh/sshd_config
+
+#CONFIGURING ONLY PUBKEY AUTHENTICATION
 sed -i -e 's/PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i -e 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+
+#ADDING GOOGLE DNS NAMESERVER
 sed -i -e 's/nameserver 0.0.0.0/nameserver 8.8.8.8/' /etc/resolv.conf
 
+#USING ETH NAMING IN GRUB
 sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="net.ifnames=0 noquiet"/' /etc/default/grub
 update-grub
 
-apt-get update -y
-apt-get install -y bridge-utils
-
+#SET UP SCRIPT FOR INTERFACES
 cat > /etc/eve-ng-interface-tuning.sh << EOF
 #!/bin/sh
 cat > /etc/network/interfaces << EOF
@@ -66,9 +77,10 @@ iface pnet1 inet static
 EOF
 chmod +x /etc/eve-ng-interface-tuning.sh
 
+#SET UP INTERFACES SERVICE
 cat > /etc/systemd/system/eve-ng-interface-tuning.service << EOF
 [Unit]
-Description=Run my custom task at shutdown
+Description=Tuning interfaces on shutdown/reboot
 DefaultDependencies=no
 Before=shutdown.target reboot.target
 
@@ -80,16 +92,21 @@ TimeoutStartSec=0
 [Install]
 WantedBy=shutdown.target reboot.target
 EOF
+systemctl enable eve-ng-interface-tuning.service
 
-systemctl enable int-tune.service
 
-
+#INSTALLING EVE-NG
 wget -O - http://www.eve-ng.net/repo/install-eve.sh | bash -i
 #after this command update clients known_hosts
 
+#CONFIGURING MASQUERADING FOR INTERNET ACCESS
 echo "1" > /proc/sys/net/ipv4/ip_forward
 iptables -A FORWARD -i pnet1 -j ACCEPT
 iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -o pnet0 -j MASQUERADE
+
+#ALLOWING DHCP (UDP:67) AND DNS (UDP:53) PORTS ON PNET1 (LAB ENVIRONMENT)
+iptables -A INPUT -i pnet1 -p udp -m udp --dport 67 -j ACCEPT
+iptables -A INPUT -i pnet1 -p udp -m udp --dport 53 -j ACCEPT
 
 apt update -y
 apt install -y dnsmasq
